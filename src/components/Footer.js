bf404fc471242e6d33b64bb567166a637b28369c
Loreto Vaquero <loret.vaquero@gmail.com>
2018-07-25 20:14:14 +0200

    copy html from oldproject

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,15 @@ import './Footer.css';
 
 class Footer extends Component {
   render () {
-    return ();
+    return (
+      <footer class="footer">
+        <div class="footer__content">
+          <p class="footer__copyrigth">Awesome profile-cards @2018</p>
+          <img src="./images/pyb-logo.png" alt="Peak-y-blinded logo" class="pyb-logo"/>
+          <a class="footer__link-adalab" href="http://adalab.es/" target="_blank"> <img class="adalab-logo" src="./images/logo-adalab.png" alt="adalab-logo"/> </a>
+        </div>
+      </footer>
+    );
   }
 }
 
