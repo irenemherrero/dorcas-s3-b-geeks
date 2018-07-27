fa0b3cf405ce2f1f4ef4fce6b19ba40a29c6015c
ElisabetAS <elisabet240498@gmail.com>
2018-07-27 19:10:57 +0200

    add component collapsible

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -4,7 +4,11 @@ class Collapsible extends Component {
   render() {
     return (
         <div className="legend__container js__dropdown-title">
-          {this.props.children}
+        <legend className="fieldset__legend">
+        <i className={this.props.icon}></i>
+        <span className="text__dark--title">{this.props.name}</span>
+        </legend>
+        <i className={this.props.arrow}></i>
         </div>
     );
   }
