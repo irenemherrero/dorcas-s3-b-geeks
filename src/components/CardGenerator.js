accb383105344b78defa7d3ad02223527fd4243e
irenemherrero <irenemherrero@gmail.com>
2018-08-07 13:30:19 +0200

    add react router

--- src/components/CardGenerator.js
+++ src/components/CardGenerator.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import previewPhoto from './images/card-image.png';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import previewPhoto from '../images/card-image.png';
 
 class CardGenerator extends Component {
   constructor(props) {
